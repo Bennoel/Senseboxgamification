587161e4c68c7e99963fb0de697343b924dec43d
MaxMoody <max.hoeltgen@web.de>
2016-12-15 19:04:41 +0100

    added Map-Buttons

--- js/game.js
+++ js/game.js
@@ -1,60 +1,22 @@
 "use strict";
 
-
 var latbox;
 var longbox;
-
 var oneWeekAgo = new Date();
-oneWeekAgo.setDate(oneWeekAgo.getDate()-7);
+  oneWeekAgo.setDate(oneWeekAgo.getDate()-7);
 var isoOneWeekAgo = oneWeekAgo.toISOString();
-
 var boxId = [];
 
-var mymap = L.map('mapid').setView([51.4, 9], 2);
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'hoelsch.e2b0812b',
-    accessToken: 'pk.eyJ1IjoiaG9lbHNjaCIsImEiOiJxblpwakZrIn0.JTTnLszkIJB11k8YEe7raQ'}).addTo(mymap);
-var popup = L.popup();
-
-
-function onMapClick(e) {
-popup
-.setLatLng(e.latlng)
-.setContent("Die Entfernung zur Sensebox beträgt: " + ((L.latLng(e.latlng).distanceTo([latbox, longbox]) / 1000).toFixed(2)) + " Km" )
-.openOn(mymap);
-// punkte einteilung
-
-    if (((L.latLng(e.latlng).distanceTo([latbox, longbox]) / 1000).toFixed(2))< 50)
-      {
-        console.log("100 punkte");
-      }
-    if (((L.latLng(e.latlng).distanceTo([latbox, longbox]) / 1000).toFixed(2))<100 && ((L.latLng(e.latlng).distanceTo([latbox, longbox]) / 1000).toFixed(2))>50)
-      {
-        console.log("50 punkte");
-      }
-    if (((L.latLng(e.latlng).distanceTo([latbox, longbox]) / 1000).toFixed(2))<150 && ((L.latLng(e.latlng).distanceTo([latbox, longbox]) / 1000).toFixed(2)) >100  )
-      {
-        console.log("10 Punkte");
-      }
-    if (((L.latLng(e.latlng).distanceTo([latbox, longbox]) / 1000).toFixed(2))>150 ){
-        console.log("keine punkte für dich");
-}
-    //reload();
-}
-
-mymap.on('click', onMapClick);
 //gibt uns alle Boxen aus, ruft die randomize funktion auf um eine Random Box rauszugeben
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $http) {
   $scope.box = [];
-
-  $scope.boxes = {};
   var gameBoxId = [];
+
   $http({
     method : "GET",
     url : "https://api.opensensemap.org/boxes?"
+
   }).then(function mySucces(response) {
       $scope.myData = response.data;
       for(var i in response.data){
@@ -64,6 +26,58 @@ app.controller('myCtrl', function($scope, $http) {
     }, function myError(response) {
       $scope.myData = response.statusText;
   });
+  var boxDistance;
+  var mymap = L.map('mapid').setView([51.4, 9], 2);
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'hoelsch.e2b0812b',
+      accessToken: 'pk.eyJ1IjoiaG9lbHNjaCIsImEiOiJxblpwakZrIn0.JTTnLszkIJB11k8YEe7raQ'}).addTo(mymap);
+  var popup = L.popup();
+
+  //boxDistance ist undefined
+  var helloPopup = L.popup().setContent(("Die Entfernung zur Sensebox beträgt: " + boxDistance + " Km" ));
+  //TO-DO
+  L.easyButton('glyphicon-ok', function(){
+      helloPopup.setLatLng(mymap.getCenter()).openOn(mymap);
+  }).addTo(mymap);
+
+  //TO-DO neue Daten geladen Nachricht  + addieren der Punkte + speichern der Punkte
+  L.easyButton('fa-repeat', function(){randomize(boxId, $http)}).addTo(mymap);
+
+  function onMapClick(e) {
+
+  popup
+  .setLatLng(e.latlng)
+  .setContent("Die Entfernung zur Sensebox beträgt: " + ((L.latLng(e.latlng).distanceTo([latbox, longbox]) / 1000).toFixed(2)) + " Km" )
+  .openOn(mymap);
+  boxDistance = (L.latLng(e.latlng).distanceTo([latbox, longbox]) / 1000).toFixed(2)
+  console.log(boxDistance);
+  // punkte einteilung
+
+      if (((L.latLng(e.latlng).distanceTo([latbox, longbox]) / 1000).toFixed(2))< 50)
+        {
+          console.log("100 punkte");
+        }
+      if (((L.latLng(e.latlng).distanceTo([latbox, longbox]) / 1000).toFixed(2))<100 && ((L.latLng(e.latlng).distanceTo([latbox, longbox]) / 1000).toFixed(2))>50)
+        {
+          console.log("50 punkte");
+        }
+      if (((L.latLng(e.latlng).distanceTo([latbox, longbox]) / 1000).toFixed(2))<150 && ((L.latLng(e.latlng).distanceTo([latbox, longbox]) / 1000).toFixed(2)) >100  )
+        {
+          console.log("10 Punkte");
+        }
+      if (((L.latLng(e.latlng).distanceTo([latbox, longbox]) / 1000).toFixed(2))>150 ){
+          console.log("keine punkte für dich");
+  }
+      //reload();
+  }
+
+  mymap.on('click', onMapClick);
+
+
+
+
   //sucht eine Randombox aus und übergibt das dann der funktion gameBox
   //übergebene boxId
   function randomize(boxId, http){
